51db3ce0fddffceed4d6b92dabebe08895a5bf54
Manh Trong Kien <manhtrongkien1901@gmail.com>
2024-10-30 20:39:49 +0700

    first commit

---
+++ js/form.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+  $(".toast").toast({
+    autohide: true,
+  });
+
+  $('button[type="submit"]').on("click", function (event) {
+    const form = $(this).closest("form")[0];
+    if (form.checkValidity() === false) {
+    } else {
+      event.preventDefault();
+      $(".toast").toast("show");
+      $("form").trigger("reset");
+      $("#preview").hide();
+      $("#preview-image").hide();
+    }
+  });
+
+  $("#file-upload").on("change", function () {
+    const file = this.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = function (e) {
+        if (
+          file.type == "image/png" ||
+          file.type == "image/jpeg" ||
+          file.type == "image/jpg"
+        ) {
+          $("#preview-image").attr("src", e.target.result).show();
+
+          $("#preview").hide();
+        } else {
+          $("#preview").attr("src", e.target.result).show();
+
+          $("#preview-image").hide();
+        }
+      };
+
+      // reader.onload = function (e) {
+      // 	if (
+      // 		file.type == "image/png" ||
+      // 		file.type == "image/jpeg" ||
+      // 		file.type == "image/jpg"
+      // 	) {
+      // 		$("#preview")
+      // 			.attr("src", e.target.result)
+      // 			.show();
+      // 	} else {
+      // 		$("#preview")
+      // 			.text("src", e.target.result)
+      // 			.show();
+      // 	}
+      // };
+
+      reader.readAsDataURL(file);
+    }
+  });
+});
